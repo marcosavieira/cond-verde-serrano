ff26d28b767f11bdad2021afad7fe75f412eea87
devmarcosav@gmail.com <devmarcosav@gmail.com>
2024-01-25 11:48:40 -0300

    feat: criando endpoint para criação de um novo lead de usuario

--- verde-serrano/routes/api.php
+++ verde-serrano/routes/api.php
@@ -4,4 +4,8 @@ use App\Http\Controllers\LeadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Rota para listar todos os leads no db
 Route::get('/lead', [LeadController::class, 'index']);
+
+//Rota para gravar um novo lead de usuario
+Route::post('/lead', [LeadController::class, 'store']);
